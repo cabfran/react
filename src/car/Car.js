e2964729fb8b6e45281698a43126709966d08376
Anatoly <trishin.anatoly2018@yandex.ru>
2019-12-20 22:09:40 +0300

    funtion

--- src/car/Car.js
+++ src/car/Car.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function Car() {
-  return (
-    <div> This is car component</div>
-  )
-}
+// function Car() {
+//   return (
+//     <div> This is car component</div>
+//   )
+// }
 
-export default Car
+// const car = () => {
+//   return (
+//      <div> This is car component</div>
+//   )
+
+// }
+
+// const car = () => <div>This is car component</div>;
+
+
+
+export default () => (
+  <div>
+    This is car component
+    <strong> Some text</strong>
+  </div>
+);
