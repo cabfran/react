fbf754b5b0d949bfcd3a4872aca5877905ed6b94
Anatoly <trishin.anatoly2018@yandex.ru>
2019-12-23 15:54:56 +0300

    smth

--- src/car/Car.js
+++ src/car/Car.js
@@ -13,8 +13,21 @@ class Car extends React.Component {
   componentWillUpdate(nextProps, nextState) {
     console.log("Car componentWillUpdate", nextProps, nextState);
   }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("Car getDerivedStateFromProps", nextProps, prevState);
+    return prevState;
+  }
+
   componentDidUpdate() {
     console.log("Car componentDidUpdate");
+  }
+  getSnapshotBeforeUpdate() {
+    console.log("Car getSnapshotBeforeUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("Car componentWillUnmount");
   }
 
   render() {
