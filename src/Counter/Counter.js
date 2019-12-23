4336e2305ec068efe2d1aaaa0d8455e8b0a8acaa
Anatoly <trishin.anatoly2018@yandex.ru>
2019-12-23 23:31:11 +0300

    create new file Auxiliary, new React tags

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Auxiliary from '../hoc/Auxillary'
 export default class Counter extends Component {
   state = {
     counter: 0
@@ -11,28 +11,28 @@ export default class Counter extends Component {
     })
   ];
   render() {
-    // return (
-    //   <div>
-    //     <h2>Counter {this.state.counter}</h2>
-    //     <button onClick={this.addCounter}>+</button>
-    //     <button
-    //       onClick={() => this.setState({ counter: this.state.counter - 1 })}
-    //     >
-    //       -
-    //     </button>
-    //   </div>
-    // );
-    return [
-      <h2 key={"1"}>Counter {this.state.counter}</h2>,
-      <button key={"2"} onClick={this.addCounter}>
-        +
-      </button>,
-      <button
-        key={"3"}
-        onClick={() => this.setState({ counter: this.state.counter - 1 })}
-      >
-        -
-      </button>
-    ];
+    return (
+      <Auxiliary>
+        <h2>Counter {this.state.counter}</h2>
+        <button onClick={this.addCounter}>+</button>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter - 1 })}
+        >
+          -
+        </button>
+      </Auxiliary>
+    );
+    // return [
+    //   <h2 key={"1"}>Counter {this.state.counter}</h2>,
+    //   <button key={"2"} onClick={this.addCounter}>
+    //     +
+    //   </button>,
+    //   <button
+    //     key={"3"}
+    //     onClick={() => this.setState({ counter: this.state.counter - 1 })}
+    //   >
+    //     -
+    //   </button>
+    // ];
   }
 }
